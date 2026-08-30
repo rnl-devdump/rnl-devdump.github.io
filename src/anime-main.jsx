66aad8a06ab01511c3814a464a0825c7b29fcbc1
rnl-devdump <rnl.incogni2@gmail.com>
2026-08-30 17:18:41 +0800

    Fix CSS loading issue on Anime platform

--- src/anime-main.jsx
+++ src/anime-main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './anime/anime.css';
 import App from './anime/AnimeApp.jsx';
 
 createRoot(document.getElementById('root')).render(
